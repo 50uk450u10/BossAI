8ba0a8895bb8da932955e2ca2e4bb22df638ce19
Dagon Zamora <5050uuk10a@gmail.com>
2024-11-22 17:48:59 -0600

    ethjwkler

--- Assets/[BadDagonJoke]/Skreepts/Mroom.cs
+++ Assets/[BadDagonJoke]/Skreepts/Mroom.cs
@@ -6,12 +6,19 @@ namespace Dagonite
 {
     public class Mroom : MonoBehaviour
     {
+        Rigidbody rb;
+        bool invisible;
         int maxHealth = 100;
         int currHealth;
+        int rangedDamage = 25;
+        int meleeDamage = 50;
+        int speed = 10;
 
         private void Start()
         {
+            rb = GetComponent<Rigidbody>();
             currHealth = maxHealth;
+            invisible = false;
         }
     }
 }
